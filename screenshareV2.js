1f7e112e52757af484f2b73af19c3e4acc2cd316
MAAX <mmr.90.90@hotmail.com>
2019-07-12 12:33:34 +0300

    Update screenshareV2.js

--- screenshareV2.js
+++ screenshareV2.js
@@ -7,18 +7,18 @@ let prefix = "!";
 
 client.on('message', message => {
 
-	if (!(message.author.bot) && message.channel.type === 'dm') return;
+  if (!(message.author.bot) && message.channel.type === 'dm') return;
 
-    if (message.content.startsWith(`${prefix}screenlink`) || message.content.startsWith(`${prefix}sl`)) {
+  if (message.content.startsWith(`${prefix}screenlink`) || message.content.startsWith(`${prefix}sl`)) {
 
     var voiceChannel = message.member.voiceChannel;
 
-	if(!voiceChannel) return message.channel.send("You must be in a room first")
+  if(!voiceChannel) return message.channel.send("You must be in a room first!")
 
     let embed = new Discord.RichEmbed ()
 	.setAuthor(`${message.guild.name}`, message.guild.iconURL)
 	.setColor('#000001')
-    .setTitle("**Screen Link**")
+        .setTitle("**Screen Link**")
 	.addField(`__This Link For__ **${message.member.voiceChannel.name}** __Room__`,`[Click here to screenshare!](https://discordapp.com/channels/${message.guild.id}/${message.member.voiceChannel.id})`)
 	.addField(`Room Name : **${message.member.voiceChannel.name}**`,`Requested By : <@${message.author.id}>`)
 	embed.setFooter(`You must be in the same channel for using the link.`, message.author.avatarURL)
